9de76f6347f66f3350f47f0abde4dc5b49f417ac
Ritu kukreja <104751465+Ritu0208@users.noreply.github.com>
2022-11-15 09:15:27 +0530

    minor update

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -18,6 +18,10 @@ export default function TextForm(props) {
   }
 
   const handleCopy = () => {
+    // var text = document.getElementById("myBox");
+    // text.select();
+    // navigator.clipboard.writeText(text.value);
+    // document.getSelection().removeAllRanges();
     
     navigator.clipboard.writeText(text);
     
